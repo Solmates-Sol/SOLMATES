766d542ab5755cecc2b4afcc390434b57ed91903
Mohit Parmar <100152343+mohitparmar1@users.noreply.github.com>
2024-03-24 13:47:10 +0530

    frontend : added mentor page

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,12 +1,13 @@
 
 import Navbar from "@/components/Navbar";
 import HomeComponent from "@/components/Home";
-
+import MentorComponent from "@/components/Mentors"
 export default function HomePage() {
     return (
         <div>
             <Navbar />
             <HomeComponent />
+			<MentorComponent />
         </div>
     );
 }
---
+++ frontend/src/components/Card.tsx
@@ -0,0 +1,13 @@
+"use client"
+import React from 'react'
+
+function Card() {
+  return (
+    <div className='w-[300px] h-80 '>
+        <h1> Mentor Name</h1>
+        <p></p>
+    </div>
+  )
+}
+
+export default Card
