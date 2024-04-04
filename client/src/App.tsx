e3cd09e6d15e47297c91c471c21545157ef58eba
kirtiraj22 <kirtirajthakor064@gmail.com>
2024-04-04 13:41:54 +0530

    frontend: sign in ui

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,14 @@
 import { Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import HomeComponent from "./components/Home";
+import SignIn from "./pages/SignIn";
 
 function App() {
 	return (
 		<Routes>
 			<Route path="/" element={<Navbar />}>
 				<Route path="/" element={<HomeComponent />} />
-				<Route path="/signin" />
+				<Route path="/signin" element={<SignIn />} />
 				<Route path="/signup" />
 			</Route>
 		</Routes>
