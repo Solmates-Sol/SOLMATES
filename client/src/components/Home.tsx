c337a5e4d985bdc0ce9be05309e845b80918bfd2
Kulrajsinh9904 <chauhankulrajsinh9904@gmail.com>
2024-04-06 15:32:12 +0530

    Frontend: Update Home Page

--- client/src/components/Home.tsx
+++ client/src/components/Home.tsx
@@ -1,28 +1,32 @@
-import Featured from "./Featured"
-import OverviewCard from "./OverviewCard";
+import React from "react";
+import aboutImage from "../assets/dev2.png";
 
-const HomeComponent = () => {
-	return (
-		<>
-			<div className="w-screen h-screen mx-auto flex flex-col justify-center items-center bg-[#fbf5e5] overflow-hidden">
-				<h1 className=" my-5 text-center text-black text-4xl font-bold">
-					Start your side hustle today
+const About: React.FC = () => {
+  return (
+    <div className="bg-[#fbf5e5] min-h-screen flex items-center">
+      <div className="container mx-auto px-6 md:px-12">
+        <div className="flex flex-col md:flex-row items-center pl-20 justify-between">
+          <div className="w-full md:w-1/2 order-2 md:order-1">
+		  <h1 className=" my-5  justify-start text-black text-4xl font-bold">
+		  Building Bonds Between  Developers
 				</h1>
-				<p className="w-1/2 text-center">
-					Turn your passion and knowledge into a thriving business.
-					Help your audience get ahead in life.
-				</p>
-				<div className="flex gap-3">
+            <p className="text-black mt-8 mb-5">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            </p>
+			<div className="flex gap-3">
 					<button className="btn-dark my-10">Get Started</button>
 					<button className="btn-light my-10 border-2">
 						Learn More
 					</button>
 				</div>
-			</div>
-			<Featured />
-			<OverviewCard />
-		</>
-	);
-};
+          </div>
+          <div className="w-50  order-1 md:order-2 pr-20">
+            <img src={aboutImage} alt="meeting" className="w-full rounded-lg" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
 
-export default HomeComponent;
+export default About;
